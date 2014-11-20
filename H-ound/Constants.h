9347c5dd98b79d335b403e0a8986e0bf8be1a5d7
Matteo Comisso <teomatteo89@gmail.com>
2014-11-20 16:55:31 +0100

    Header file with constants

---
+++ H-ound/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  H-ound Fart Ed.
+//
+//  Created by Matteo Comisso on 20/11/14.
+//  Copyright (c) 2014 Blue-Mate. All rights reserved.
+//
+
+#ifndef H_ound_Fart_Ed__Constants_h
+#define H_ound_Fart_Ed__Constants_h
+
+
+#endif
